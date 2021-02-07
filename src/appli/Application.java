cdbda73326434cd821fec52168a4ba6c8398ca6d
Amande F <amandine.douguedroit@gmail.com>
2021-02-07 16:31:49 +0100

    C7 Methode utile au domaine de gestion : DONE !

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -1,60 +1,12 @@
 package appli;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Calendar;
-
-import dao.Dao;
-import metier.programmation.Representation;
 
 public class Application {
-
-	/**
-	 * Rercherche, par theatre et par salle, des dates sur lesquelles aucune 
-	 * représentation n'est programmée
-	 * 
-	 * @return (<b><i>datesDispo</i></b>) la liste des dates disponibles
-	 */
-	public ArrayList<LocalDate> rechercherDatesDispo() {
-		
-		// je recupere les dates entrees par l'utilisateur (pour l'instant en dur)
-		//TODO recuperer les dates entrées par l'utilisateur sur la page adminTheatre.jsp
-		int endDay = 8;
-		ArrayList<LocalDate> dates = new ArrayList<LocalDate>();
-		LocalDate start = LocalDate.of(2021, 4, 1);
-		LocalDate end = LocalDate.of(2021, 2, endDay + 1);
-		dates = getDatesRange(start, end);
-		
-		// je recupere les dates des representations 
-		ArrayList<Representation> representations = Dao.representations;			// Recupere la liste des represenations dans Dao
-		ArrayList<LocalDate> planningRepres = new ArrayList<LocalDate>();			// Declare une nouvelle liste de dates
-		for (Representation repres : representations) {								
-			LocalDateTime planning = repres.getPlanning();							
-			int year = planning.getYear();
-			int month = planning.getMonthValue();
-			int day = planning.getDayOfMonth();
-			LocalDate planningDate = LocalDate.of(year, month, day);
-			planningRepres.add(planningDate);										// Ajoute les dates de representations dans la 
-		}																			// nouvelle liste
-		
-		
-		// Je cherche les dates de l'intervalle pour lesquelles il n'y a pas de representation et je les mets dans une liste
-		ArrayList<LocalDate> datesDispo = new ArrayList<LocalDate>();
-		for (LocalDate intervalleDate : dates) {
-			for (LocalDate date : planningRepres)  {
-				if(!date.isEqual(intervalleDate)) {
-					datesDispo.add(intervalleDate);
-				}
-			}
-		}
-		
-		System.out.println(datesDispo);
-		return datesDispo;
-	}
 	
 	/**
-	 * Generation des dates qui se situent dans un intervalle donne
+	 * Generation des dates qui se situent dans un intervalle de dates donne
 	 * 
 	 * @param start	date de debut de periode
 	 * @param end	date de fin de periode
--- src/metier/programmation/Salle.java
+++ src/metier/programmation/Salle.java
@@ -1,10 +1,49 @@
 package metier.programmation;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import appli.Application;
+import dao.Dao;
+
 public class Salle {
 
 	// prorpietes d'instance
 	private String nom;
 	private int jauge;
+	
+	// methode
+	/**
+	 * Rercherche, par theatre et par salle, des dates sur lesquelles aucune 
+	 * représentation n'est programmée
+	 * 
+	 * @return (<b><i>datesDispo</i></b>) la liste des dates disponibles
+	 */
+	public ArrayList<LocalDate> rechercherDatesDispo() {
+		
+		// je recupere l'intervalle de dates entre par l'utilisateur (pour l'instant en dur)
+		//TODO recuperer l'intervalle entre par l'utilisateur sur la page adminTheatre.jsp
+		int endDay = 8;
+		LocalDate start = LocalDate.of(2021, 4, 1);
+		LocalDate end = LocalDate.of(2021, 4, endDay + 1);
+		ArrayList<LocalDate> interval = Application.getDatesRange(start, end);
+
+		// je recupere les dates des representations dans la dao
+		ArrayList<Representation> representations = Dao.representations;	
+
+		// pour chaque date de représentation, je cherche la date de l'intervalle qui lui correspond et je la supprime de la liste des dates de l'intervalle
+		for (Representation representation : representations) {								
+			LocalDate representationDate = representation.getPlanning().toLocalDate();
+			for (int i = 0; i <= interval.size(); i++) {
+				if (representationDate.isEqual(interval.get(i))) {
+					interval.remove(i);
+					break;
+				}
+			}
+		}
+		
+		return interval;
+	}
 	
 	// getters
 	public String getNom() {
