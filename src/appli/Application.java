b4f29fa50d0bbc36af47179bd3c1690443edeeb0
Amande F <amandine.douguedroit@gmail.com>
2021-02-07 13:07:57 +0100

    C2 Application - Modification et ajout de la méthode getDatesRange()

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -1,5 +1,6 @@
 package appli;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -14,11 +15,33 @@ public class Application {
 	 */
 	public ArrayList<LocalDateTime> rechercherDatesDispo() {
 		
-		// je recupere le calendrier 
+		// je recupere les dates entrees par l'utilisateur
+		//TODO recuperer les dates entrées par l'utilisateur sur la page adminTheatre.jsp
 		
-		Calendar calendar = Calendar.getInstance();
 		
 		ArrayList<LocalDateTime> datesDispo = new ArrayList<LocalDateTime>();
 		return datesDispo;
 	}
+	
+	/**
+	 * Generation des dates qui se situent dans un intervalle donne
+	 * 
+	 * @param start	date de debut de periode
+	 * @param end	date de fin de periode
+	 * 
+	 * @return (<b><i>datesRange</i></b>) la liste des dates qui se situent entre <b><i>start</i></b> et <b><i>end</i></b>
+	 */
+	public static ArrayList<LocalDate> datesRange (LocalDate start, LocalDate end) {
+		
+		ArrayList<LocalDate> dates = new ArrayList<LocalDate>();
+		
+		LocalDate localDate = start;
+		while (localDate.isBefore(end)) {
+		    dates.add(localDate);
+		    // Set up the next loop.
+		    localDate = localDate.plusDays(1);
+		}
+		return dates;
+	}
+	
 }
