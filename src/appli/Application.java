dfecd49f35e0ddd4b681549defec84f7bf6fb762
Amande F <amandine.douguedroit@gmail.com>
2021-02-07 14:36:17 +0100

    C4 Application - boucle bouclée mais non fonctionnelle

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -5,6 +5,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import dao.Dao;
+import metier.programmation.Representation;
+
 public class Application {
 
 	/**
@@ -13,20 +16,40 @@ public class Application {
 	 * 
 	 * @return (<b><i>datesDispo</i></b>) la liste des dates disponibles
 	 */
-	public ArrayList<LocalDateTime> rechercherDatesDispo() {
+	public ArrayList<LocalDate> rechercherDatesDispo() {
 		
 		// je recupere les dates entrees par l'utilisateur (pour l'instant en dur)
 		//TODO recuperer les dates entrées par l'utilisateur sur la page adminTheatre.jsp
-		int endDay = 7;
+		int endDay = 8;
 		ArrayList<LocalDate> dates = new ArrayList<LocalDate>();
-		LocalDate start = LocalDate.of(2021, 1, 1);
+		LocalDate start = LocalDate.of(2021, 4, 1);
 		LocalDate end = LocalDate.of(2021, 2, endDay + 1);
 		dates = getDatesRange(start, end);
 		
 		// je recupere les dates des representations 
+		ArrayList<Representation> representations = Dao.representations;			// Recupere la liste des represenations dans Dao
+		ArrayList<LocalDate> planningRepres = new ArrayList<LocalDate>();			// Declare une nouvelle liste de dates
+		for (Representation repres : representations) {								
+			LocalDateTime planning = repres.getPlanning();							
+			int year = planning.getYear();
+			int month = planning.getMonthValue();
+			int day = planning.getDayOfMonth();
+			LocalDate planningDate = LocalDate.of(year, month, day);
+			planningRepres.add(planningDate);										// Ajoute les dates de representations dans la 
+		}																			// nouvelle liste
 		
 		
-		ArrayList<LocalDateTime> datesDispo = new ArrayList<LocalDateTime>();
+		// Je cherche les dates de l'intervalle pour lesquelles il n'y a pas de representation et je les mets dans une liste
+		ArrayList<LocalDate> datesDispo = new ArrayList<LocalDate>();
+		for (LocalDate intervalleDate : dates) {
+			for (LocalDate date : planningRepres)  {
+				if(!date.isEqual(intervalleDate)) {
+					datesDispo.add(intervalleDate);
+				}
+			}
+		}
+		
+		System.out.println(datesDispo);
 		return datesDispo;
 	}
 	
@@ -40,15 +63,15 @@ public class Application {
 	 */
 	public static ArrayList<LocalDate> getDatesRange (LocalDate start, LocalDate end) {
 		
-		ArrayList<LocalDate> dates = new ArrayList<LocalDate>();
+		ArrayList<LocalDate> datesRange = new ArrayList<LocalDate>();
 		
 		LocalDate localDate = start;
 		while (localDate.isBefore(end)) {
-		    dates.add(localDate);
+		    datesRange.add(localDate);
 		    // Set up the next loop.
 		    localDate = localDate.plusDays(1);
 		}
-		return dates;
+		return datesRange;
 	}
 	
 }
