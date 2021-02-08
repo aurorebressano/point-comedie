c897420a5cf8c0bfa56a79e5e7c37af0f822ac2e
Amande F <amandine.douguedroit@gmail.com>
2021-02-08 22:33:53 +0100

    C10 Spectacle - push revu

--- src/metier/programmation/Spectacle.java
+++ src/metier/programmation/Spectacle.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import java.util.ArrayList;
 
-public abstract class Spectacle {
+public class Spectacle {
 
 	// propriétés d'instance
 	private String titre;
@@ -13,13 +13,8 @@ public abstract class Spectacle {
 	private String duree;
 	private String cheminImage;
 	private ArrayList<Artiste> artistes;
-<<<<<<< HEAD:src/metier/programmation/Spectacle.java
 	private Representations representations;
-
-=======
-	private Salle salle;
 	
->>>>>>> be1fbfe4d9ae11ce78ce9bc0df8df66ea37846fe:src/metier/Spectacle.java
 	// getters
 	public String getTitre() {
 		return titre;
@@ -39,17 +34,10 @@ public abstract class Spectacle {
 	public ArrayList<Artiste> getArtistes() {
 		return artistes;
 	}
-<<<<<<< HEAD:src/metier/programmation/Spectacle.java
 	public Representations getRepresentations() {
 		return representations;
 	}
 
-=======
-	public Salle getSalle() {
-		return salle;
-	}
-	
->>>>>>> be1fbfe4d9ae11ce78ce9bc0df8df66ea37846fe:src/metier/Spectacle.java
 	// setters
 	public void setTitre(String titre) {
 		this.titre = titre;
@@ -69,18 +57,8 @@ public abstract class Spectacle {
 	public void setArtistes(ArrayList<Artiste> artistes) {
 		this.artistes = artistes;
 	}
-<<<<<<< HEAD:src/metier/programmation/Spectacle.java
 	public void setRepresentations(Representations representations) {
 		this.representations = representations;
 	}
 
-
 }
-=======
-	public void setSalle(Salle salle) {
-		this.salle = salle;
-	}
-	
-	
-}
->>>>>>> be1fbfe4d9ae11ce78ce9bc0df8df66ea37846fe:src/metier/Spectacle.java
